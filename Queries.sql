8a2c6dacbda312dd88d8da4cf701856720543021
Ben Cooper <bassbencooper999@gmail.com>
2020-04-22 22:46:24 +0100

    Finished more questions

--- Queries.sql
+++ Queries.sql
@@ -18,20 +18,20 @@ INSERT INTO passenger (passenger_id, first_name, last_name, passport_no, nationa
 --2
 --a)
 --Should delete lead customer where customer ID = 3
-
+DELETE FROM lead_customer WHERE customer_id = 3;
 --b)
 --Run task 8 for booking id 504
 
 --c)
 --Delete lead cutomer id 3
-
+DELETE FROM lead_customer WHERE customer_id = 3;
 --3
 --a)
 SELECT flight.flight_id, flight.flight_date, max_capacity, COALESCE(num_seats,0) AS "Booked Seats", flight.max_capacity - COALESCE(flight_booking.num_seats,0) AS "Avaliable seats" FROM flight LEFT JOIN flight_booking ON flight.flight_id = flight_booking.flight_id WHERE flight_booking.status = 'r' OR flight_booking.status is null GROUP BY flight.flight_id, num_seats;
 
 --b)
 --Should be for flight 103
-SELECT flight.flight_id, flight.flight_date, max_capacity, COALESCE(num_seats,0) AS "Booked Seats", SUM(flight.max_capacity) - SUM(flight_booking.num_seats) AS "Avaliable seats" FROM flight LEFT JOIN flight_booking ON flight.flight_id = flight_booking.flight_id WHERE flight.flight_id = '110' GROUP BY flight.flight_id, num_seats;
+SELECT flight.flight_id, flight.flight_date, max_capacity, COALESCE(num_seats,0) AS "Booked Seats", flight.max_capacity - COALESCE(flight_booking.num_seats,0) AS "Avaliable seats" FROM flight LEFT JOIN flight_booking ON flight.flight_id = flight_booking.flight_id WHERE flight_booking.status = 'r' OR flight_booking.status is null AND flight.flight_id = 103 GROUP BY flight.flight_id, num_seats;
 
 --c)
 --Should be for destination BRS
@@ -45,8 +45,25 @@ SELECT flight.flight_id, seat_booking.seat_number, flight_booking.status FROM fl
 
 --5
 --Ranked list of all lead customers from highest total cost to lowest
+SELECT lead_customer.customer_id, CONCAT(first_name , ' ' , last_name ) AS "Full Name", count(flight_booking.customer_id) AS "Number of Bookings", SUM (flight_booking.total_cost) AS "Total Spent" FROM lead_customer, flight_booking WHERE lead_customer.customer_id = flight_booking.customer_id GROUP BY lead_customer.customer_id ORDER BY SUM(flight_booking.total_cost) DESC;
 
 --6
 --a)
 
+--7
+--a)
+INSERT INTO seat_booking(booking_id, passenger_id, seat_number) VALUES (510, 1024, '8A');
+
+--b)
+INSERT INTO seat_booking(booking_id, passenger_id, seat_number) VALUES (510, 1025, '8B');
+
+--8
+--a)
+SELECT * FROM flight_booking WHERE booking_id = 509;
+
+--b)
+SELECT flight.flight_id, seat_booking.seat_number, flight_booking.status FROM flight, flight_booking, seat_booking WHERE flight.flight_id = flight_booking.flight_id AND flight_booking.booking_id = seat_booking.booking_id AND flight.flight_id = 504;
+
+--c)
+SELECT lead_customer.customer_id, CONCAT(first_name , ' ' , last_name ) AS "Full Name", count(flight_booking.customer_id) AS "Number of Bookings", SUM (flight_booking.total_cost) AS "Total Spent" FROM lead_customer, flight_booking WHERE lead_customer.customer_id = flight_booking.customer_id GROUP BY lead_customer.customer_id ORDER BY SUM(flight_booking.total_cost) DESC;
 
